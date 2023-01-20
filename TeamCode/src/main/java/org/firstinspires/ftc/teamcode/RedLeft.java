9344215c834183ce216067657965426411e0f9d3
GabyG0ldberg <103542521+GabyG0ldberg@users.noreply.github.com>
2023-01-19 22:55:41 -0500

    updates on teleop and claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeft.java
@@ -34,8 +34,8 @@ public class RedLeft extends LinearOpMode {
     int LEFT = 12;
     int MIDDLE = 16;
     int RIGHT = 5;
-    Rev2mDistanceSensor ds1;
-    Rev2mDistanceSensor ds2;
+    //Rev2mDistanceSensor ds1;
+    //Rev2mDistanceSensor ds2;
     //ColorSensor cs;
     OpenCvWebcam camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
@@ -67,10 +67,10 @@ public class RedLeft extends LinearOpMode {
         //t = telemetry.addData("count").get(Telemetry.class, "t");
         ls = hardwareMap.get(DcMotor.class, "ls");
         cl = hardwareMap.get(Servo.class, "cl");
-        ds1 = hardwareMap.get(Rev2mDistanceSensor.class, "ds1");
-        ds2 = hardwareMap.get(Rev2mDistanceSensor.class, "ds2");
+       // ds1 = hardwareMap.get(Rev2mDistanceSensor.class, "ds1");
+        //ds2 = hardwareMap.get(Rev2mDistanceSensor.class, "ds2");
         //cs = hardwareMap.get(ColorSensor.class, "cs");
-        everything = new HackHers_Lib(fL, fR, bL, bR, ls, cl, ds1, ds2, camera);
+        everything = new HackHers_Lib(fL, fR, bL, bR, ls, cl, camera);
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.getAll(WebcamName.class).get(0), cameraMonitorViewId);
         aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
