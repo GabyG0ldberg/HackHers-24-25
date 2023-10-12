17699d530ac49a17dc4484ee2255f9b0706f1039
Maria <102193979+HeyItsMaria@users.noreply.github.com>
2023-10-12 15:51:16 -0400

    Update MosaicDetectorExampleRED.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/MosaicDetectorExampleRED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/MosaicDetectorExampleRED.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.auton;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -24,6 +24,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 @TeleOp
 public class MosaicDetectorExampleRED extends LinearOpMode
 {
+
     OpenCvInternalCamera camera;
     MosaicDeterminationPipeline pipeline;
 
@@ -78,6 +79,25 @@ public class MosaicDetectorExampleRED extends LinearOpMode
 
     public static class MosaicDeterminationPipeline extends OpenCvPipeline
     {
+        static final Rect LeftROI = new Rect(
+                new Point(30,290),
+                new Point(79,335));
+        static final Rect MiddleROI = new Rect(
+                new Point(345,290),
+                new Point(375,380));
+        static final Rect RightROI = new Rect(
+                new Point(450,290),
+                new Point(79,335));
+
+        static double PERCENT_COLOR_THRESHOLD = 0.4;
+
+        Telemetry telemetry;
+        Mat frame = new Mat();
+        public enum Location {
+            LEFT,
+            RIGHT,
+            NOT_FOUND
+        }
         /*
          * An enum to define the skystone position
          */
@@ -88,113 +108,47 @@ public class MosaicDetectorExampleRED extends LinearOpMode
             RIGHT
         }
 
-        /*
-         * Some color constants
-         */
-        static final Scalar BLUE = new Scalar(0, 0, 255);
-        static final Scalar GREEN = new Scalar(0, 255, 0);
 
-        /*
-         * The core values which define the location and size of the sample regions
-         */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(30,290);
-        static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(345,290);
-        static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(450,290);
-
-       //static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(109,98);
-       //            static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(181,98);
-       //            static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(253,98);
-        static final int REGION_WIDTH = 49;
-        static final int REGION_HEIGHT = 45;
-
-        /*
-         * Points which actually define the sample region rectangles, derived from above values
-         *
-         * Example of how points A and B work to define a rectangle
-         *
-         *   ------------------------------------
-         *   | (0,0) Point A                    |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                                  |
-         *   |                  Point B (70,50) |
-         *   ------------------------------------
-         *
-         */
-        Point region1_pointA = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x,
-                REGION1_TOPLEFT_ANCHOR_POINT.y);
-        Point region1_pointB = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-        Point region2_pointA = new Point(
-                REGION2_TOPLEFT_ANCHOR_POINT.x,
-                REGION2_TOPLEFT_ANCHOR_POINT.y);
-        Point region2_pointB = new Point(
-                REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-        Point region3_pointA = new Point(
-                REGION3_TOPLEFT_ANCHOR_POINT.x,
-                REGION3_TOPLEFT_ANCHOR_POINT.y);
-        Point region3_pointB = new Point(
-                REGION3_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION3_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-        /*
-         * Working variables
-         */
-        Mat region1_Cr, region2_Cr, region3_Cr;
-        Mat frame = new Mat();
-        final Mat hsvMat = new Mat();
-        //Mat Cr = new Mat();
-        int avg1, avg2, avg3;
-
-        // Volatile since accessed by OpMode thread w/o synchronization
-        private volatile SkystonePosition position = SkystonePosition.LEFT;
-
-        /*
-         * This function takes the RGB frame, converts to YCrCb,
-         * and extracts the Cb channel to the 'Cb' variable
-         */
-        void inputToCr(Mat input)
-        {
-            Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
-            Scalar lowerRed = new Scalar(85, 50, 40);         // lower color border for RED
-            Scalar upperRed = new Scalar(135, 255, 255);
-            Core.inRange(hsvMat, lowerRed, upperRed, frame);
-
-        }
 
         @Override
         public void init(Mat firstFrame)
         {
-            /*
-             * We need to call this in order to make sure the 'Cb'
-             * object is initialized, so that the submats we make
-             * will still be linked to it on subsequent frames. (If
-             * the object were to only be initialized in processFrame,
-             * then the submats would become delinked because the backing
-             * buffer would be re-allocated the first time a real frame
-             * was crunched)
-             */
-            inputToCr(firstFrame);
 
-            /*
-             * Submats are a persistent reference to a region of the parent
-             * buffer. Any changes to the child affect the parent, and the
-             * reverse also holds true.
-             */
-            region1_Cr = frame.submat(new Rect(region1_pointA, region1_pointB));
-            region2_Cr = frame.submat(new Rect(region2_pointA, region2_pointB));
-            region3_Cr = frame.submat(new Rect(region3_pointA, region3_pointB));
         }
-
+       // public MosaicDetectorExampleRED(Telemetry t) { telemetry = t; }
         @Override
-        public Mat processFrame(Mat input)
-        {
+        public Mat processFrame(Mat input) {
+
+
+            Imgproc.cvtColor(input, frame, Imgproc.COLOR_RGB2HSV);
+            Scalar lowerRed = new Scalar(0, 94, 87);         // lower color border for RED
+            Scalar upperRed = new Scalar(0, 68, 100);
+            Core.inRange(frame, lowerRed, upperRed, frame);
+
+            Mat left = frame.submat(LeftROI);
+            Mat right = frame.submat(RightROI);
+            Mat middle = frame.submat(MiddleROI);
+            double leftValue = Core.sumElems(left).val[0] / LeftROI.area() / 255;
+            double rightValue = Core.sumElems(right).val[0] / RightROI.area() / 255;
+            double middleValue = Core.sumElems(right).val[0] / MiddleROI.area() / 255;
+
+            left.release();
+            right.release();
+            middle.release();
+
+            telemetry.addData("Middle raw value", (int) Core.sumElems(middle).val[0]);
+            telemetry.addData("Left raw value", (int) Core.sumElems(left).val[0]);
+            telemetry.addData("Right raw value", (int) Core.sumElems(right).val[0]);
+            telemetry.addData("Left percentage", Math.round(leftValue * 100) + "%");
+            telemetry.addData("Right percentage", Math.round(rightValue * 100) + "%");
+            telemetry.addData("Middle percentage", Math.round(middleValue * 100) + "%");
+
+            boolean stoneLeft = leftValue > PERCENT_COLOR_THRESHOLD;
+            boolean stoneRight = rightValue > PERCENT_COLOR_THRESHOLD;
+            boolean stoneMiddle = middleValue > PERCENT_COLOR_THRESHOLD;
+
+            if (stoneLeft)
+
             /*
              * Overview of what we're doing:
              *
@@ -232,125 +186,9 @@ public class MosaicDetectorExampleRED extends LinearOpMode
 
             /*
              * Get the Cb channel of the input frame after conversion to YCrCb
-             */
-            inputToCr(input);
 
-            /*
-             * Compute the average pixel value of each submat region. We're
-             * taking the average of a single channel buffer, so the value
-             * we need is at index 0. We could have also taken the average
-             * pixel value of the 3-channel image, and referenced the value
-             * at index 2 here.
-             */
-            avg1 = (int) Core.mean(region1_Cr).val[0];
-            avg2 = (int) Core.mean(region2_Cr).val[0];
-            avg3 = (int) Core.mean(region3_Cr).val[0];
-
-            /*
-             * Draw a rectangle showing sample region 1 on the screen.
-             * Simply a visual aid. Serves no functional purpose.
-             */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region1_pointA, // First point which defines the rectangle
-                    region1_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
-
-            /*
-             * Draw a rectangle showing sample region 2 on the screen.
-             * Simply a visual aid. Serves no functional purpose.
-             */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region2_pointA, // First point which defines the rectangle
-                    region2_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
-
-            /*
-             * Draw a rectangle showing sample region 3 on the screen.
-             * Simply a visual aid. Serves no functional purpose.
-             */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region3_pointA, // First point which defines the rectangle
-                    region3_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
+        }     */
 
 
-            /*
-             * Find the max of the 3 averages
-             */
-            int maxOneTwo = Math.max(avg1, avg2);
-            int max = Math.max(maxOneTwo, avg3);
-
-            /*
-             * Now that we found the max, we actually need to go and
-             * figure out which sample region that value was from
-             */
-            if(max == avg1) // Was it from region 1?
-            {
-                position = SkystonePosition.LEFT; // Record our analysis
-
-                /*
-                 * Draw a solid rectangle on top of the chosen region.
-                 * Simply a visual aid. Serves no functional purpose.
-                 */
-                Imgproc.rectangle(
-                        input, // Buffer to draw on
-                        region1_pointA, // First point which defines the rectangle
-                        region1_pointB, // Second point which defines the rectangle
-                        GREEN, // The color the rectangle is drawn in
-                        -1); // Negative thickness means solid fill
-            }
-            else if(max == avg2) // Was it from region 2?
-            {
-                position = SkystonePosition.CENTER; // Record our analysis
-
-                /*
-                 * Draw a solid rectangle on top of the chosen region.
-                 * Simply a visual aid. Serves no functional purpose.
-                 */
-                Imgproc.rectangle(
-                        input, // Buffer to draw on
-                        region2_pointA, // First point which defines the rectangle
-                        region2_pointB, // Second point which defines the rectangle
-                        GREEN, // The color the rectangle is drawn in
-                        -1); // Negative thickness means solid fill
-            }
-            else if(max == avg3) // Was it from region 3?
-            {
-                position = SkystonePosition.RIGHT; // Record our analysis
-
-                /*
-                 * Draw a solid rectangle on top of the chosen region.
-                 * Simply a visual aid. Serves no functional purpose.
-                 */
-                Imgproc.rectangle(
-                        input, // Buffer to draw on
-                        region3_pointA, // First point which defines the rectangle
-                        region3_pointB, // Second point which defines the rectangle
-                        GREEN, // The color the rectangle is drawn in
-                        -1); // Negative thickness means solid fill
-            }
-
-            /*
-             * Render the 'input' buffer to the viewport. But note this is not
-             * simply rendering the raw camera feed, because we called functions
-             * to add some annotations to this buffer earlier up.
-             */
-            return input;
-        }
-
-        /*
-         * Call this from the OpMode thread to obtain the latest analysis
-         */
-        public SkystonePosition getAnalysis()
-        {
-            return position;
-        }
-    }
 }
 
