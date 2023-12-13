a5a78f995524dc4cb1384cb193212d4e454a70a5
GabyG0ldberg <103542521+GabyG0ldberg@users.noreply.github.com>
2023-12-12 20:07:18 -0500

    Got enconders working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GabyTestJr.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GabyTestJr.java
@@ -3,6 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import java.io.BufferedInputStream;
@@ -13,32 +14,71 @@ import java.io.BufferedInputStream;
 @Autonomous(name="GabyTestJr")
 public class GabyTestJr extends LinearOpMode {
     private HackHers_Lib everything;
-    DcMotor ls;
-    Servo cl;
+    DcMotorEx fL;
+    DcMotorEx fR;
+    DcMotorEx bL;
+    DcMotorEx bR;
 
-
+//ls.setDirection(DcMotor.Direction.REVERSE);
 
     @Override
     public void runOpMode() throws InterruptedException {
-        ls = hardwareMap.get(DcMotor.class, "ls");
-        ls.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        ls.setDirection(DcMotor.Direction.REVERSE);
-        ls.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  // reset encoder counts kept by motors.
-        ls.setTargetPosition(3500);  // set motors to run forward for 3500 encoder counts.
-        ls.setMode(DcMotor.RunMode.RUN_TO_POSITION); // set motors to run to target encoder position and stop with brakes on.
+        DcMotorEx fL = hardwareMap.get(DcMotorEx.class, "fL");
+        DcMotorEx fR = hardwareMap.get(DcMotorEx.class, "fR");
+        DcMotorEx bL = hardwareMap.get(DcMotorEx.class, "bl");
+        DcMotorEx bR = hardwareMap.get(DcMotorEx.class, "bR");
+
+
+        fL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        fL.setDirection(DcMotor.Direction.REVERSE);
+        fR.setDirection(DcMotor.Direction.REVERSE);
+        bL.setDirection(DcMotor.Direction.REVERSE);
+        bR.setDirection(DcMotor.Direction.REVERSE);
+
+
+        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  // reset encoder counts kept by motors.
+        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  // reset encoder counts kept by motors.
+        bL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  // reset encoder counts kept by motors.
+        bR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  // reset encoder counts kept by motors.
+
+        fL.setTargetPosition(3500);  // set motors to run forward for 3500 encoder counts.
+        fR.setTargetPosition(3500);  // set motors to run forward for 3500 encoder counts.
+        bL.setTargetPosition(3500);  // set motors to run forward for 3500 encoder counts.
+        bR.setTargetPosition(3500);  // set motors to run forward for 3500 encoder counts.
+
+        fL.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // set motors to run to target encoder position and stop with brakes on.
+        fR.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // set motors to run to target encoder position and stop with brakes on
+        bL.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // set motors to run to target encoder position and stop with brakes on.
+        bR.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // set motors to run to target encoder position and stop with brakes on
+
+
         telemetry.addData("Mode", "waiting");
         telemetry.update();
         waitForStart();// wait for start button.
         telemetry.addData("Mode", "running");
         telemetry.update();
-        ls.setPower(0.6);
-        while (ls.getCurrentPosition() < ls.getTargetPosition())  //fL.getCurrentPosition() < fL.getTargetPosition() //opModeIsActive() && ls.isBusy()
+        fL.setVelocity(1000);
+        fR.setVelocity(1000);
+        bL.setVelocity(1000);
+        bR.setVelocity(1000);
+        //fR.setPower(0.05);
+        while (Math.abs(fL.getCurrentPosition()) < Math.abs(fL.getTargetPosition()) && Math.abs(fR.getCurrentPosition()) < Math.abs(fR.getTargetPosition()))  //fL.getCurrentPosition() < fL.getTargetPosition() //opModeIsActive() && ls.isBusy()
         {
-            telemetry.addData("encoder-linear-slides", ls.getCurrentPosition() + "  busy=" + ls.isBusy());
+            telemetry.addData("encoder-linear-slides", fL.getCurrentPosition() + "  busy=" + fL.isBusy());
+            telemetry.addData("encoder-linear-slides", fR.getCurrentPosition() + "  busy=" + fR.isBusy());
+            telemetry.addData("encoder-linear-slides", bL.getCurrentPosition() + "  busy=" + bL.isBusy());
+            telemetry.addData("encoder-linear-slides", bR.getCurrentPosition() + "  busy=" + bR.isBusy());
             telemetry.update();
             idle();
         }
-        ls.setPower(0);
+        fL.setVelocity(0);
+        fR.setVelocity(0);
+        bL.setVelocity(0);
+        bR.setVelocity(0);
 
 
         //return null;
